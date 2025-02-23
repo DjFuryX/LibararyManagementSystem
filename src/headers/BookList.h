4cfe9ba0d8340aebafe4b2f40a918f26e52bf04c
hasani548 <124756408+hasani548@users.noreply.github.com>
2025-02-23 00:58:20 -0500

    Patron&BookCreated

---
+++ src/headers/BookList.h
@@ -0,0 +1,50 @@
+#include <string>
+#include <iostream>
+#include "Book.h"
+
+using namespace std;
+
+//Used to store collection of Books
+
+
+// TO DO !!
+// Implement Node then add in BST for Sorting
+
+class BookList{
+    private:
+
+        Node* head;
+
+    public:
+    //constructor
+
+    BookList():head(nullptr){}
+
+   
+   
+    //Add Book
+    void addBook (Book, book){
+        Node* newNode = new Node(book);
+        if(!head){
+            head = newNode;
+        }
+        else {
+            Node* temp = head;
+            while (temp->nextNode){
+                temp=nextNode;
+            }
+        }
+    }
+
+
+    //Display all 
+
+    void displayBookInfo(){
+        Node* temp = head; //start at head
+        
+        while(temp != nullptr){// then traversse 
+            temp->data.displayBookInfo();
+            temp=temp->nextNode;
+        }
+    }
+}
