480c37e0a9861acd11ea9bcf70e4b2a18a4f16eb
Norman Martin <normaneverthonmartin@gmail.com>
2025-03-22 03:47:38 -0500

    change main screen to login

--- src/headers/GUI/sceneManager.h
+++ src/headers/GUI/sceneManager.h
@@ -45,7 +45,7 @@ public:
         admin = new Admin;
 
         // first scene that is shown
-        current = welcome;
+        current = patronLogin;
 
         filemanager.ReadData(&library);
         // library.GetPatronList()->DisplayList();
